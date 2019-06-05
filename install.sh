7ba3710e6fd813ce43c715fe71d789c9e37ac753
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2019-06-05 07:47:40 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+# Authors:      Rodrigo Cuadra
+#               with Collaboration of Jose Miguel Rivera
+#
+# Support:      rcuadra@vitalpbx.com
+#
+echo -e "\n"
+echo -e "************************************************************"
+echo -e "*      Welcome to the VitalPBX Google TTS Integration      *"
+echo -e "************************************************************"
+yum install -y perl perl-LWP-Protocol-https mpg123 sox perl-libwww-perl
+cd /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/Google_TTS_VitalPBX/master/googletts.agi
+chown asterisk:asterisk googletts.agi
+chmod +x googletts.agi
+cd /etc/asterisk/ombutel
+wget https://raw.githubusercontent.com/VitalPBX/Google_TTS_VitalPBX/master/extensions__60-google_tts.conf
+asterisk -rx"dialplan reload"
+echo -e "\n"
+echo -e "************************************************************"
+echo -e "*               For test dial 1234 or 4321                 *"
+echo -e "************************************************************"
